63a9657a83a144e5aef6331c5f04f5ba75183b50
Vallasc <giacomovallorani.97@gmail.com>
2022-01-05 14:34:11 +0100

    Deleted

--- src/utils.ts
+++
@@ -1,9 +0,0 @@
-import * as THREE from "three"
-
-export class Utils {
-    static readonly UP = new THREE.Vector3(0, 0, 1)
-    static readonly LEFT = new THREE.Vector3(-1, 0, 0)
-    static readonly TOP = new THREE.Vector3(0, 1, 0)
-    static readonly RIGHT = new THREE.Vector3(1, 0, 0)
-    static readonly BOTTOM = new THREE.Vector3(0, -1, 0)
-}
