90d417f3fec9dcf7cf373996032836c1f29862a3
Vallasc <giacomovallorani.97@gmail.com>
2022-01-14 11:18:25 +0100

    Add awareness and provider configs

--- src/pages/App.svelte
+++ src/pages/App.svelte
@@ -2,16 +2,31 @@
 	import Menu from './Menu.svelte'
 	import Game  from './Game.svelte'
 	import { WebrtcProvider } from 'y-webrtc'
+	import * as awarenessProtocol from "y-protocols/awareness";
 	import * as Y from 'yjs'
     import { pacmanName } from '../store.js'
+	import { onDestroy } from 'svelte'
 
-	const version = "version 0.1.4"
+	const version = "version 0.1.6"
 
 	console.log(version)
 
-	const ydoc: Y.Doc = new Y.Doc({gc: true})
-	const provider: WebrtcProvider = new WebrtcProvider( 'distribuited-pacman', ydoc)
-	provider.maxConns = 10
+	onDestroy(()=>{
+		provider.disconnect()
+	})
+
+	const garbageCollector = true
+	const ydoc: Y.Doc = new Y.Doc({gc: garbageCollector})
+	const awareness = new awarenessProtocol.Awareness(ydoc)
+	const provider: WebrtcProvider = new WebrtcProvider( 'distribuited-pacman', ydoc, {
+		signaling: ['wss://signaling.yjs.dev'],
+		password: null,
+		awareness: awareness,
+		maxConns: 10,
+		filterBcConns: true,
+		peerOpts: {}
+	})
+
 
 </script>
 
