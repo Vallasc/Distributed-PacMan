be644d783f731329b5db5ac8e8bafbf05045fe9b
Vallasc <giacomovallorani.97@gmail.com>
2022-01-14 11:16:20 +0100

    Add isAlive and dieAnimation

--- src/game/pacman.ts
+++ src/game/pacman.ts
@@ -2,40 +2,43 @@ import * as THREE from "three"
 import type { KeyState } from "./keyboard"
 import type { World } from "./world"
 import type { GameState } from "./state"
-import { Utils } from "./utils"
+import { Mesh, Utils } from "./utils"
 import { FontLoader } from 'three/examples/jsm/loaders/FontLoader'
 import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry'
 import JsonFont from "../font/Press_Start_2P_Regular.json";
+import type { Ghost } from "./ghost"
 
 export class Pacman {
-    static readonly PACMAN_SPEED = 2
+    static readonly PACMAN_SPEED = 2.5
     static readonly PACMAN_RADIUS = 0.4
 
-    static readonly PACMAN_ONLINE_MATERIAL = new THREE.MeshPhongMaterial({ color: 'yellow', side: THREE.DoubleSide })
-    static readonly PACMAN_OFFLINE_MATERIAL = new THREE.MeshPhongMaterial({ color: 'gray', side: THREE.DoubleSide })
+    static readonly PACMAN_ONLINE_MATERIAL = new THREE.MeshLambertMaterial({ color: 'yellow', side: THREE.DoubleSide })
+    static readonly PACMAN_OFFLINE_MATERIAL = new THREE.MeshLambertMaterial({ color: 'gray', side: THREE.DoubleSide })
 
     public id: string
     public name: string
-    public peerId: string
+    public peerId: number
 
     // true if the player press start button
     public isPlaying: boolean
     // true if the player has tab open
     public isOnline: boolean
 
-    private mesh: THREE.Mesh
-    private textMesh: THREE.Mesh
-    private frames: Array<THREE.SphereGeometry>
+    // true if the pacman is alive
+    public isAlive: boolean
+    public lostTime: number
+
+    public mesh: Mesh
+    public textMesh: THREE.Mesh
+    public frames: Array<THREE.SphereGeometry>
     // public color
 
-    private distanceMoved: number
-    private lastDistanceMoved: number
+    public distanceMoved: number
     public direction: THREE.Vector3
     public isMoving: boolean
-    public frameCounter: number
     //public deletedFlag: number = 0
 
-    constructor(id: string, name: string, peerId: string, position: THREE.Vector3) {
+    constructor(id: string, name: string, peerId: number, position: THREE.Vector3) {
         this.id = id
         this.name = name
         this.isPlaying = false
@@ -54,30 +57,37 @@ export class Pacman {
 
         let pacmanMaterial = Pacman.PACMAN_ONLINE_MATERIAL
 
-        this.mesh = new THREE.Mesh(pacmanGeometries[0], pacmanMaterial)
+        this.mesh = new Mesh(pacmanGeometries[0], pacmanMaterial)
         this.frames = pacmanGeometries
 
         this.distanceMoved = 0
-        this.lastDistanceMoved = 0
 
         // Initialize pacman facing to the left.
         this.mesh.position.copy(position)
         this.direction = new THREE.Vector3(-1, 0, 0)
         
-        this.frameCounter = 0
         this.isMoving = false
+        this.isAlive = true
+        this.mesh.isPacman = true
     }
 
     // Update pacman mesh simulating the eat movement
-    public updateFrame(currentPacman: Pacman) {
-        // Show eating animation based on how much pacman has moved.
-        let maxAngle = Math.PI / 4
-        let angle = (this.distanceMoved * 2) % (maxAngle * 2)
-        if (angle > maxAngle)
-            angle = maxAngle * 2 - angle
-        let frame = Math.floor(angle / Math.PI * this.frames.length)
-    
-        this.mesh.geometry = this.frames[frame]
+    public update(currentPacman: Pacman, timeNow: number) {
+        // Animate model
+        let frame: number
+        if (!this.isAlive) {
+            // if pacman got eaten, show dying animation
+            let angle = (timeNow - this.lostTime) * Math.PI / 2;
+            frame = Math.min(this.frames.length - 1, Math.floor(angle / Math.PI * this.frames.length));
+        } else {
+            // show eating animation based on how much pacman has moved
+            let maxAngle = Math.PI / 4
+            let angle = (this.distanceMoved * 2) % (maxAngle * 2)
+            if (angle > maxAngle)
+                angle = maxAngle * 2 - angle
+            frame = Math.floor(angle / Math.PI * this.frames.length)
+        }
+            this.mesh.geometry = this.frames[frame]
 
         // Update rotation based on direction so that mouth is always facing forward.
         // The "mouth" part is on the side of the sphere, make it "look" up but
@@ -89,6 +99,11 @@ export class Pacman {
             this.mesh.material = Pacman.PACMAN_OFFLINE_MATERIAL
             if(this.textMesh){
                 this.textMesh.material = Pacman.PACMAN_OFFLINE_MATERIAL
+            }
+        } else {
+            this.mesh.material = Pacman.PACMAN_ONLINE_MATERIAL
+            if(this.textMesh){
+                this.textMesh.material = Pacman.PACMAN_ONLINE_MATERIAL
             }
         }
 
@@ -103,8 +118,15 @@ export class Pacman {
         }
     }
 
+    public moveFake(delta: number){
+        if(this.isMoving){
+            this.mesh.translateOnAxis(this.direction, delta * Pacman.PACMAN_SPEED)
+            // TODO WALL
+        }
+    }
+
     // Elaborate key pressed and change pacman position
-    public movePacman(delta: number, keys: KeyState, levelMap: World, state: GameState) {
+    public move(delta: number, keys: KeyState, levelMap: World) {
     
         this.isMoving = false
         // Move based on current keys being pressed.
@@ -152,19 +174,27 @@ export class Pacman {
         // Wrap packman to level map
         levelMap.wrapObject(this.mesh)
     
-        // Eat dots
+    }
+
+    public eatDots(state: GameState){
         let x = Math.round(this.mesh.position.x), y = Math.round(this.mesh.position.y)
         let dot = state.getDot(x, y)
-        if(dot != null){
+        if(dot != null) {
             dot.pacmanId = this.id
             state.updateDotShared(dot)
         }
-    
     }
 
-    // Get pacman position
-    public getPosition(): THREE.Vector3 {
-        return this.mesh.position
+    public checkGhostCollision(ghost: Ghost, timeNow: number) {
+        if(this.isAlive && Utils.distance(this.mesh.position, ghost.mesh.position) < Pacman.PACMAN_RADIUS*2){
+            console.log("YOU DIED")
+            this.isAlive = false
+            this.lostTime = timeNow
+        }
+    }
+
+    public dieAnimation() {
+
     }
 
     // Add mesh to 3d scene
@@ -205,6 +235,7 @@ export class Pacman {
         obj["isMoving"] = this.isMoving
         obj["isPlaying"] = this.isPlaying
         obj["isOnline"] = this.isOnline
+        obj["isAlive"] = this.isAlive
         return obj
     }
 
@@ -218,12 +249,13 @@ export class Pacman {
 
     // Copy from a js plain object
     public copyObj(obj: any) {    
-        this.mesh.position.copy(new THREE.Vector3(obj["position"][0], obj["position"][1], obj["position"][2]))
-        this.direction.copy(new THREE.Vector3(obj["direction"][0], obj["direction"][1], obj["direction"][2]))
+        this.mesh.position.set(obj["position"][0], obj["position"][1], obj["position"][2])
+        this.direction.set(obj["direction"][0], obj["direction"][1], obj["direction"][2])
         this.distanceMoved = obj["distanceMoved"]
         this.isMoving = obj["isMoving"]
         this.isPlaying = obj["isPlaying"]
         this.isOnline = obj["isOnline"]
+        this.isAlive = obj["isAlive"]
     }
 
     // Copy object if it has the same id
