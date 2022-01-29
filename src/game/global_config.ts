60f566fdeace8c3eb0dcb6b8610b451bb2ab266f
Giacomo Vallorani <giacomovallorani.97@gmail.com>
2022-01-29 12:03:45 +0100

    Add ERROR_PROBABILITY_GHOST

--- src/game/global_config.ts
+++ src/game/global_config.ts
@@ -1,5 +1,4 @@
 export class GlobalConfig {
-
     static readonly SCATTER_TIME = 12000
     static readonly START_TIME = 5000
 
@@ -7,4 +6,5 @@ export class GlobalConfig {
     static readonly SCORE_POWER_DOT = 300
     static readonly SCORE_GHOST = 600
     static readonly MAX_PLAYERS = 8
+    static readonly ERROR_PROBABILITY_GHOST = 0.3
 }
