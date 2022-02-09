65c2986ac33cee3d5c613968c30d41307a25d4ab
Giacomo Vallorani <giacomovallorani.97@gmail.com>
2022-02-09 15:50:20 +0100

    Add disconnectSignal

--- src/store.ts
+++ src/store.ts
@@ -1,6 +1,8 @@
-import { Writable, writable } from 'svelte/store';
-import type { GameState } from './game/state';
+import { Writable, writable } from 'svelte/store'
+import type { GameState } from './game/state'
 
-export const pacmanName: Writable<string> = writable("");
-export const pacmanId: Writable<string> = writable("");
-export const globalState: Writable<null | GameState> = writable(null);
+export const pacmanName: Writable<string> = writable("")
+export const pacmanId: Writable<string> = writable("")
+export const globalState: Writable<null | GameState> = writable(null)
+
+export const disconnectSignal: Writable<boolean> = writable(true)
