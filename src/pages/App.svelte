1fdfc3e00766ff5b23366684ae6b92a7cc426ea0
Giacomo Vallorani <giacomovallorani.97@gmail.com>
2022-02-09 15:51:09 +0100

    Add debug mode

--- src/pages/App.svelte
+++ src/pages/App.svelte
@@ -8,6 +8,7 @@
 	import { onDestroy } from 'svelte'
 	import { GlobalConfig } from '../game/global_config';
 	import { Utils } from '../game/utils';
+    import Test from './Test.svelte';
 
 	const version = "1.5.6"
 
@@ -21,7 +22,6 @@
     let roomId = urlParams.get("room");
 	if( roomId == null ) {
 		roomId = Utils.getRandomString()
-		//urlParams.set("room", roomId)
 		window.history.pushState(null, null, "?room=" + roomId)
 	}
 	console.log("Room id: " + roomId)
@@ -29,18 +29,35 @@
 	const garbageCollector = true
 	const ydoc: Y.Doc = new Y.Doc({gc: garbageCollector})
 	const awareness = new awarenessProtocol.Awareness(ydoc)
-	// Disable awareness
-	awareness.destroy()
+
+	let password = roomId
+	if (location.protocol !== 'https:') {
+    	password = null
+}
 	const provider: WebrtcProvider = new WebrtcProvider( 'distribuited_pacman_' + roomId, ydoc, {
 		signaling: ['wss://signaling.yjs.dev'],
-		password: null,
+		password: password,
 		awareness: awareness,
 		maxConns: GlobalConfig.MAX_PLAYERS,
 		filterBcConns: true,
-		peerOpts: {}
+		peerOpts: {
+			config: { iceServers: [{ urls: 'stun:stun.l.google.com:19302' }]}
+		}
+	})
+
+
+	console.log(provider)
+	let debug : boolean = urlParams.get("debug") === "true";
+
+	provider.awareness.on('update', value => {
+		console.log(provider.awareness.states.keys())
 	})
 
 </script>
+
+{#if debug}
+	<Test provider={provider}></Test>
+{/if}
 
 {#if $pacmanName != ""}
 	<Game {ydoc}></Game>
