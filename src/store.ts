a926dc08d5d01b2892485b8f7636e5ff847f658a
Giacomo Vallorani <giacomovallorani.97@gmail.com>
2022-02-15 19:05:23 +0100

    Add mute

--- src/store.ts
+++ src/store.ts
@@ -4,5 +4,5 @@ import type { GameState } from './game/state'
 export const pacmanName: Writable<string> = writable("")
 export const pacmanId: Writable<string> = writable("")
 export const globalState: Writable<null | GameState> = writable(null)
-
+export const mute: Writable<boolean> = writable(false)
 export const disconnectSignal: Writable<boolean> = writable(true)
